9ed1f28c0fffa6ee439b3a186e4802fb26e6582d
DONGGUN LEE <d0n99un17@gmail.com>
2019-10-29 16:06:51 -0700

    Designing the Task List Screen

---
+++ GoodList/Controller/TaskViewController.swift
@@ -0,0 +1,31 @@
+//
+//  TaskViewController.swift
+//  GoodList
+//
+//  Created by DONGGUN LEE on 10/29/19.
+//  Copyright © 2019 DONGGUN LEE. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class TaskViewController: UIViewController, UITableViewDelegate, UITableViewDataSource{
+    
+    @IBOutlet weak var prioritySegmentControl: UISegmentedControl!
+    
+    @IBOutlet weak var toboTableView: UITableView!
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TaskTableViewCell", for: indexPath)
+        
+        return cell
+    }
+    
+    
+
+    
+}
